da55122ecd7a92a5fd82cee06b4d7569dc6f777b
Lei Shi <shileiwill@gmail.com>
2016-07-17 14:36:19 -0500

    subsets with duplicates

---
+++ Round1/src/chapter1/strstr/subset/SubsetsWithDup.java
@@ -0,0 +1,41 @@
+package chapter1.strstr.subset;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class SubsetsWithDup {
+
+	public static void main(String[] args) {
+
+	}
+
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        List<Integer> list = new ArrayList<Integer>();
+        
+        if (nums == null) {
+            return null;
+        }
+        
+        Arrays.sort(nums); // Sort and reconigize duplicate elements easily
+        helper(res, list, nums, 0);
+        
+        return res;
+    }
+    
+    private void helper(List<List<Integer>> res, List<Integer> list, int[] nums, int pos) {
+    	res.add(new ArrayList<Integer>(list)); // Remember to deep copy the list
+        
+        for (int i = pos; i < nums.length; i++) { // Dont repeat yourself. Start from a new point always.
+            // If 2 elements are same, and if the first element is not selected, then next ones should be ignored.
+            if (i != pos && nums[i] == nums[i - 1]) {
+            	// i != pos, means we executed for loop at least once, and we removed the previous element(not selected)
+                continue;
+            }
+            list.add(nums[i]);
+            helper(res, list, nums, i + 1); // In order to keep moving, here is i+1, rather than pos+1
+            list.remove(list.size() - 1);
+        }
+    }
+}

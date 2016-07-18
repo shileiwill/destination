e1760e00c78db842e6f1d5db37b0abaaa9e14a19
Lei Shi <shileiwill@gmail.com>
2016-07-17 19:36:41 -0500

    Letter combinations of phone numbers

---
+++ Round1/src/chapter1/strstr/subset/LetterCombinations.java
@@ -0,0 +1,59 @@
+package chapter1.strstr.subset;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class LetterCombinations {
+
+	public static void main(String[] args) {
+
+	}
+	
+    public List<String> letterCombinations(String digits) {
+        Map<Character, char[]> map = buildMap(); // Key type must be Character. Array can be char[]
+        List<String> res = new ArrayList<String>();
+        
+        if (digits == null || digits.length() == 0) {
+            return res;
+        }
+        // Compare with other subsets questions, this is StringBuilder, rather than List
+        StringBuilder sb = new StringBuilder();
+        helper(res, sb, digits, map);
+        
+        return res;
+    }
+    
+    private void helper(List<String> res, StringBuilder sb, String digits, Map<Character, char[]> map) {
+        
+        int length = sb.length();
+        if (length == digits.length()) { // When achieve desired length, done!
+            res.add(sb.toString());
+            return;
+        }
+        
+        char nextDigit = digits.charAt(length); // Get next digit, based on the length of dynamic StringBuilder
+        char[] letters = map.get(nextDigit); // Get the list of chars
+        for (char c : letters) { // Iterate and backtrack the list
+            sb.append(c);
+            helper(res, sb, digits, map);
+            sb.deleteCharAt(sb.length() - 1);
+        }
+    }
+    
+    private Map<Character, char[]> buildMap() {
+        Map<Character, char[]> map = new HashMap<Character, char[]>();
+        
+        map.put('2', new char[]{'a', 'b', 'c'});
+        map.put('3', new char[]{'d', 'e', 'f'});
+        map.put('4', new char[]{'g', 'h', 'i'});
+        map.put('5', new char[]{'j', 'k', 'l'});
+        map.put('6', new char[]{'m', 'n', 'o'});
+        map.put('7', new char[]{'p', 'q', 'r', 's'});
+        map.put('8', new char[]{'t', 'u', 'v'});
+        map.put('9', new char[]{'w', 'x', 'y', 'z'});
+        
+        return map;
+    }
+}

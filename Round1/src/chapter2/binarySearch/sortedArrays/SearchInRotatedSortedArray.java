ca0a3180288739a540e20c421541400552e87a2b
Lei Shi <shileiwill@gmail.com>
2016-07-18 23:03:05 -0500

    Rotated Sorted Array

---
+++ Round1/src/chapter2/binarySearch/sortedArrays/SearchInRotatedSortedArray.java
@@ -0,0 +1,49 @@
+package chapter2.binarySearch.sortedArrays;
+/**
+ * 33. Suppose a sorted array is rotated at some pivot unknown to you beforehand.
+
+(i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+
+You are given a target value to search. If found in the array return its index, otherwise return -1.
+
+You may assume no duplicate exists in the array.
+ * @author Lei
+ *
+ */
+public class SearchInRotatedSortedArray {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        
+        while (left + 1 < right) {
+            int mid = left + (right - left) / 2;
+            
+            if (nums[mid] == target) {
+                return mid;
+            }
+            
+            if (nums[mid] > nums[0]) {// Left hand in order
+                if (target < nums[mid] && target >= nums[0]) { // Stay on left
+                    right = mid;
+                } else { // Go to the disorder
+                    left = mid;
+                }
+            } else { // Right in order
+                if (target > nums[mid] && target <= nums[nums.length - 1]) { // Stay on right
+                    left = mid;
+                } else { // Go to the disorder
+                    right = mid;
+                }
+            }
+        }
+        
+        if (nums[left] == target) {
+            return left;
+        }
+        if (nums[right] == target) {
+            return right;
+        }
+        
+        return -1;
+    }
+}
---
+++ Round1/src/chapter2/binarySearch/sortedArrays/SearchInRotatedSortedArray2.java
@@ -0,0 +1,51 @@
+package chapter2.binarySearch.sortedArrays;
+/**
+ * 81. What if duplicates are allowed?
+ * @author Lei
+ *
+ */
+public class SearchInRotatedSortedArray2 {
+    public boolean search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        
+        while (left + 1 < right) {
+            int mid = left + (right - left) / 2;
+            
+            if (nums[mid] == target) {
+                return true;
+            }
+            
+            // Use left, right. Dont use 0, nums.length - 1, as left right ends are changing
+            if (nums[mid] == nums[left] && nums[mid] == nums[right]) { // Search both sides, just decrease the range
+                left = left + 1;
+                right = right - 1;
+            } else if (nums[mid] == nums[left]) { // Left are all the same, no doubt, go to right
+                left = mid;
+            } else if (nums[mid] == nums[right]) {
+                right = mid;                
+            } else if (nums[mid] > nums[left]) {// Left hand in order
+                if (target < nums[mid] && target >= nums[left]) { // Stay on left
+                    right = mid;
+                } else { // Go to the disorder
+                    left = mid;
+                }
+            } else { // Right in order
+                if (target > nums[mid] && target <= nums[right]) { // Stay on right
+                    left = mid;
+                } else { // Go to the disorder
+                    right = mid;
+                }
+            }
+        }
+        
+        if (nums[left] == target) {
+            return true;
+        }
+        if (nums[right] == target) {
+            return true;
+        }
+        
+        return false;
+    }
+}
